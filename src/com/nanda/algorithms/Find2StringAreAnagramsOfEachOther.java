985148a114f1c7c0dab18706863068cca7662c5b
Nandakumar.Menon <nandakumar82@gmail.com>
2016-11-20 10:48:03 +0530

    Find2StringAreAnagramsOfEachOther

---
+++ src/com/nanda/algorithms/Find2StringAreAnagramsOfEachOther.java
@@ -0,0 +1,54 @@
+package com.nanda.algorithms;
+
+/**
+ * Created by Nandakumar.Menon on 18-11-2016.
+ */
+/*
+ * We increment the count of each character in the first array and
+ * decrement the count of each character in the second array.
+ *
+ * If the resulting counts array is full of zeros, then strings are anagrams otherwise not.
+ */
+public class Find2StringAreAnagramsOfEachOther {
+
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = "abc";
+        System.out.println("Anagram?? :"+isAnagram(str1, str2));
+    }
+
+    private static boolean isAnagram(String a, String b){
+
+        //if both string is null, then considering it as anagram.
+        if(a==b){
+            return true;
+        }
+
+        //if any one string is null, then they are not anagram.
+        if(a==null || b==null)
+            return false;
+
+        //If length of both strings are not same then obviously they are not anagrams.
+        if(a.length()!=b.length())
+            return false;
+
+        char[] aArr = a.toLowerCase().toCharArray();
+        char[] bArr = b.toLowerCase().toCharArray();
+
+        // An array to hold the number of occurrences of each character
+        int[] counts = new int[26];
+
+        for (int i = 0; i < aArr.length; i++){
+            counts[aArr[i]-97]++;  // Increment the count of the character at respective position
+            counts[bArr[i]-97]--;  // Decrement the count of the character at respective position
+        }
+
+        // If the strings are anagrams, then counts array will be full of zeros not otherwise
+        for (int i = 0; i<26; i++){
+            if (counts[i] != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
