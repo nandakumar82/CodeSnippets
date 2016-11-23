5093b7192b22318165946754738b148c9f79ae67
Nandakumar.Menon <nandakumar82@gmail.com>
2016-11-23 10:29:31 +0530

    FindLargestSmallestNumberArray

---
+++ src/com/nanda/array/FindLargestSmallestNumberArray.java
@@ -0,0 +1,32 @@
+package com.nanda.array;
+
+/**
+ * Created by Nandakumar.Menon on 22-11-2016.
+ */
+public class FindLargestSmallestNumberArray {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{10,1,2,8,3,15,3};
+        findLargestSmallestNumberArray(arr);
+    }
+
+    private static void findLargestSmallestNumberArray(int[] arr){
+        if(arr==null || arr.length < 1){
+            return;
+        }
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int value : arr) {
+            if(value < min){
+                min = value;
+            }
+            if(value > max){
+                max = value;
+            }
+        }
+        System.out.println("Minimum element :"+min);
+        System.out.println("Maximum element :"+max);
+    }
+}
+
