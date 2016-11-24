2a41e5184fbd085c7f108216e2b5788bae8e368a
Nandakumar.Menon <nandakumar82@gmail.com>
2016-11-24 14:56:25 +0530

    QueueUsingStackApp

---
+++ src/com/nanda/stack/QueueUsingStackApp.java
@@ -0,0 +1,55 @@
+package com.nanda.stack;
+
+import java.util.Stack;
+
+/**
+ * Created by Nandakumar.Menon on 24-11-2016.
+ */
+public class QueueUsingStackApp {
+    public static void main(String[] args) {
+        QueueUsingSingleStack queueUsingStack = new QueueUsingSingleStack();
+
+        queueUsingStack.enQueue(10);
+        queueUsingStack.enQueue(20);
+        queueUsingStack.enQueue(30);
+        queueUsingStack.enQueue(40);
+
+        queueUsingStack.deQueue();
+        queueUsingStack.deQueue();
+
+        queueUsingStack.enQueue(50);
+        queueUsingStack.enQueue(60);
+
+        queueUsingStack.deQueue();
+        queueUsingStack.deQueue();
+        queueUsingStack.deQueue();
+        queueUsingStack.deQueue();
+        queueUsingStack.deQueue();
+        queueUsingStack.deQueue();
+        queueUsingStack.deQueue();
+    }
+}
+
+class QueueUsingSingleStack{
+    Stack<Integer> stack = new Stack<Integer>();
+
+    public void enQueue(int data){
+        stack.add(data);
+    }
+
+    public void deQueue(){
+        if(stack.size()<1){
+            System.out.println("Nothing to deQueue");
+            return;
+        }
+
+        if(stack.size()==1){
+            System.out.println(stack.pop());
+            return;
+        }
+
+        int data = stack.pop();
+        deQueue();
+        stack.push(data);
+    }
+}
