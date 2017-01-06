2b73053034e48467a700e31cf3de9a1ad71a6853
Nandakumar.Menon <nandakumar82@gmail.com>
2017-01-06 08:30:15 +0530

    DuplicateCharsInString

---
+++ src/com/nanda/algorithms/DuplicateCharsInString.java
@@ -0,0 +1,35 @@
+package com.nanda.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Nandakumar.Menon on 04-01-2017.
+ */
+public class DuplicateCharsInString {
+
+    public void findDuplicateChars(String str){
+
+        Map<Character, Integer> dupMap = new HashMap<Character, Integer>();
+        char[] chrs = str.toCharArray();
+        for(Character ch:chrs){
+            if(dupMap.containsKey(ch)){
+                dupMap.put(ch, dupMap.get(ch)+1);
+            } else {
+                dupMap.put(ch, 1);
+            }
+        }
+        Set<Character> keys = dupMap.keySet();
+        for(Character ch:keys){
+            if(dupMap.get(ch) > 1){
+                System.out.println(ch+"--->"+dupMap.get(ch));
+            }
+        }
+    }
+
+    public static void main(String a[]){
+        DuplicateCharsInString dcs = new DuplicateCharsInString();
+        dcs.findDuplicateChars("Java2Novice");
+    }
+}
