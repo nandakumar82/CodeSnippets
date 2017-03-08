72473b82cada247b1ef7030d9a1e5f2f604e8784
Nandakumar.Menon <nandakumar82@gmail.com>
2017-03-08 10:50:12 +0530

    MyNumberSumRec

---
+++ src/com/nanda/algorithms/MyNumberSumRec.java
@@ -0,0 +1,25 @@
+package com.nanda.algorithms;
+
+/**
+ * Created by Nandakumar.Menon on 04-01-2017.
+ */
+public class MyNumberSumRec {
+
+    int sum = 0;
+
+    public int getNumberSum(int number){
+
+        if(number == 0){
+            return sum;
+        } else {
+            sum += (number%10);
+            getNumberSum(number/10);
+        }
+        return sum;
+    }
+
+    public static void main(String a[]){
+        MyNumberSumRec mns = new MyNumberSumRec();
+        System.out.println("Sum is: "+mns.getNumberSum(223));
+    }
+}
