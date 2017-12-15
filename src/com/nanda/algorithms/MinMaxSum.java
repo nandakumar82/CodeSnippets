68049f9ffb3c71e8635983bf9da5e3f57faf78bd
Nandakumar.Menon <nandakumar82@gmail.com>
2017-12-15 08:49:21 +0530

    MinMaxSum

---
+++ src/com/nanda/algorithms/MinMaxSum.java
@@ -0,0 +1,24 @@
+package com.nanda.algorithms;
+
+import java.util.Scanner;
+
+/**
+ * Created by nandakumar.menon on 09-07-2017.
+ */
+public class MinMaxSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long[] nums = new long[5];
+
+        long max = 0, min= 0 , sum =0;
+        nums[0] = max = min = sum = in.nextLong(); //Read the first value outside the loop, to handle min calculation
+        for (int i = 1; i < 5; i++) {
+            nums[i] = in.nextLong();
+            if(nums[i]>max) max = nums[i];
+            if(nums[i]<min) min = nums[i];
+            sum += nums[i];
+        }
+        System.out.println( (sum - max) + " " + (sum - min));
+
+    }
+}
