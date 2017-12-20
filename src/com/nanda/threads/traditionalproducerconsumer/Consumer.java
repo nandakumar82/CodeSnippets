9c62f9569464a5c735f4185e435ea013d248a0ec
Nandakumar.Menon <nandakumar82@gmail.com>
2017-12-20 11:50:31 +0530

    Consumer

---
+++ src/com/nanda/threads/traditionalproducerconsumer/Consumer.java
@@ -0,0 +1,47 @@
+package com.nanda.threads.traditionalproducerconsumer;
+
+import java.util.List;
+
+/**
+ * Created by nandakumar.menon on 16-04-2017.
+ */
+class Consumer implements Runnable
+{
+    private final List<Integer> taskQueue;
+
+    public Consumer(List<Integer> sharedQueue)
+    {
+        this.taskQueue = sharedQueue;
+    }
+
+    @Override
+    public void run()
+    {
+        while (true)
+        {
+            try
+            {
+                consume();
+            } catch (InterruptedException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void consume() throws InterruptedException
+    {
+        synchronized (taskQueue)
+        {
+            while (taskQueue.isEmpty())
+            {
+                System.out.println("Queue is empty " + Thread.currentThread().getName() + " is waiting , size: " + taskQueue.size());
+                taskQueue.wait();
+            }
+            Thread.sleep(1000);
+            int i = taskQueue.remove(0);
+            System.out.println("Consumed: " + i);
+            taskQueue.notifyAll();
+        }
+    }
+}
