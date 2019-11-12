81d400d4c6e7edd21ade9aab85c07648dd364b00
Nandak <nandak@nextgen.com>
2019-11-12 13:40:50 +0530

    Consumer

--- src/com/nanda/threads/traditionalproducerconsumer/Consumer.java
+++ src/com/nanda/threads/traditionalproducerconsumer/Consumer.java
@@ -5,36 +5,27 @@ import java.util.List;
 /**
  * Created by nandakumar.menon on 16-04-2017.
  */
-class Consumer implements Runnable
-{
+class Consumer implements Runnable {
     private final List<Integer> taskQueue;
 
-    public Consumer(List<Integer> sharedQueue)
-    {
+    public Consumer(List<Integer> sharedQueue) {
         this.taskQueue = sharedQueue;
     }
 
     @Override
-    public void run()
-    {
-        while (true)
-        {
-            try
-            {
+    public void run() {
+        while (true) {
+            try {
                 consume();
-            } catch (InterruptedException ex)
-            {
+            } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
         }
     }
 
-    private void consume() throws InterruptedException
-    {
-        synchronized (taskQueue)
-        {
-            while (taskQueue.isEmpty())
-            {
+    private void consume() throws InterruptedException {
+        synchronized (taskQueue) {
+            while (taskQueue.isEmpty()) {
                 System.out.println("Queue is empty " + Thread.currentThread().getName() + " is waiting , size: " + taskQueue.size());
                 taskQueue.wait();
             }
